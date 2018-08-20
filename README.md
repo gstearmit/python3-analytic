263aef5ddbb63be40a7b0fb26c7cb4f987d8a256
gstearmit <gstearmit@gmail.com>
2018-08-20 15:13:58 +0700

    first commit V1.0.0.0

---
+++ README.md
@@ -0,0 +1,47 @@
+1. $ python --version
+Python 3.6.5 :: Anaconda, Inc.
+
+2. $ virtualenv analytics
+-bash: virtualenv: command not found
+
+3. $ pip install virtualenv
+Collecting virtualenv
+  Downloading https://files.pythonhosted.org/packages/b6/30/96a02b2287098b23b875bc8c2f58071c35d2efe84f747b64d523721dc2b5/virtualenv-16.0.0-py2.py3-none-any.whl (1.9MB)
+    100% |████████████████████████████████| 1.9MB 1.1MB/s
+distributed 1.21.8 requires msgpack, which is not installed.
+Installing collected packages: virtualenv
+Successfully installed virtualenv-16.0.0
+
+4. $ virtualenv analytics
+Using base prefix '/anaconda3'
+New python executable in /Users/weshop.dev/analytics/bin/python
+Installing setuptools, pip, wheel...done.
+
+
+5. $ cd analytics/
+6. $ source bin/activate
+(analytics) weshopdev:analytics weshop.dev$ pip install flask peewee
+Collecting flask
+  Downloading https://files.pythonhosted.org/packages/7f/e7/08578774ed4536d3242b14dacb4696386634607af824ea997202cd0edb4b/Flask-1.0.2-py2.py3-none-any.whl (91kB)
+    100% |████████████████████████████████| 92kB 716kB/s
+Collecting peewee
+
+
+7. $ pip install gevent
+Collecting gevent
+  Downloading https://files.pythonhosted.org/packages/2b/a9/7c38605b9672a6ede6ccf822a645fdeec0c80fb467c87c5ce4976e4056dd/gevent-1.3.6-cp36-cp36m-macosx_10_6_intel.whl (3.1MB)
+    100% |████████████████████████████████| 3.1MB 979kB/s
+
+8. $ vi analytics.py
+
+9. $ DEBUG=1 python analytics.py
+ * Serving Flask app "analytics" (lazy loading)
+ * Environment: production
+   WARNING: Do not use the development server in a production environment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 258-079-966
+
---
+++ analytics.py
@@ -0,0 +1,107 @@
+from base64 import b64decode
+import datetime
+import json
+import os
+from urllib.parse   import parse_qsl, urlparse
+
+from flask import Flask, Response, abort, request
+from peewee import *
+
+
+# 1 pixel GIF, base64-encoded.
+BEACON = b64decode('R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==')
+
+# Store the database file in the app directory.
+APP_DIR = os.path.dirname(__file__)
+DATABASE_NAME = os.path.join(APP_DIR, 'analytics.db')
+DOMAIN = 'http://127.0.0.1:5000'  # TODO: change me.
+
+# Simple JavaScript which will be included and executed on the client-side.
+#JAVASCRIPT = '' # TODO: add javascript implementation.
+JAVASCRIPT = """(function(){
+    var d=document,i=new Image,e=encodeURIComponent;
+    i.src='%s/a.gif?url='+e(d.location.href)+'&ref='+e(d.referrer)+'&t='+e(d.title);
+    })()""".replace('\n', '')
+
+
+
+# Flask application settings.
+DEBUG = bool(os.environ.get('DEBUG'))
+SECRET_KEY = 'secret - change me'  # TODO: change me.
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+database = SqliteDatabase(DATABASE_NAME, pragmas={
+    'journal_mode': 'wal',  # WAL-mode for better concurrent access.
+    'cache_size': -32000})  # 32MB page cache.
+
+
+class JSONField(TextField):
+    """Store JSON data in a TextField."""
+    def python_value(self, value):
+        if value is not None:
+            return json.loads(value)
+
+    def db_value(self, value):
+        if value is not None:
+            return json.dumps(value)
+
+
+
+class PageView(Model):
+    # TODO: add model definition.
+    domain = CharField()
+    url = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+    title = TextField(default='')
+    ip = CharField(default='')
+    referrer = TextField(default='')
+    headers = JSONField()
+    params = JSONField()
+
+    @classmethod
+    def create_from_request(cls):
+        parsed = urlparse(request.args['url'])
+        params = dict(parse_qsl(parsed.query))
+
+        return PageView.create(
+            domain=parsed.netloc,
+            url=parsed.path,
+            title=request.args.get('t') or '',
+            ip=request.headers.get('X-Forwarded-For', request.remote_addr),
+            referrer=request.args.get('ref') or '',
+            headers=dict(request.headers),
+            params=params)
+
+    class Meta:
+        database = database
+
+@app.route('/a.gif')
+def analyze():
+    # TODO: implement 1pixel gif view.
+    if not request.args.get('url'):
+        abort(404)
+
+    with database.transaction():
+        PageView.create_from_request()
+
+    response = Response(app.config['BEACON'], mimetype='image/gif')
+    response.headers['Cache-Control'] = 'private, no-cache'
+    return response
+
+@app.route('/a.js')
+def script():
+    # TODO: implement javascript view.
+    return Response(
+        app.config['JAVASCRIPT'] % (app.config['DOMAIN']),
+        mimetype='text/javascript')
+    
+
+@app.errorhandler(404)
+def not_found(e):
+    return Response('Not found.')
+
+if __name__ == '__main__':
+    database.create_tables([PageView], safe=True)
+    app.run()
